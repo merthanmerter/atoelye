65ea87764cb00f7a6a4be2a9b7779ba16644b393
merthanmerter <merthanm@gmail.com>
2025-04-22 20:24:45 +0300

    Update next-intl dependency to canary version and refactor Next.js configuration for internationalization support.

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,8 @@
-import { NextConfig } from "next";
-import createNextIntlPlugin from "next-intl/plugin";
+import type { NextConfig } from "next";
+import NextIntlPlugin from "next-intl/plugin";
 
-const nextConfig: NextConfig = {
-  turbopack: {
-    resolveExtensions: [".ts", ".tsx", ".js", ".jsx", ".json"],
-  },
-};
+const withNextIntl = NextIntlPlugin();
 
-const withNextIntl = createNextIntlPlugin();
+const nextConfig: NextConfig = {};
+
 export default withNextIntl(nextConfig);
