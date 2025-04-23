7846429cf02f101e0d7654e2c0789e7e644c38d3
merthanmerter <merthanm@gmail.com>
2025-04-24 01:59:00 +0300

    Remove API route file and correct import path in rpc.ts to align with updated routing structure.

--- src/lib/rpc.ts
+++ src/lib/rpc.ts
@@ -1,4 +1,4 @@
-import { AppRouter } from "@/app/api/[[...all]}/route";
+import { AppRouter } from "@/app/api/[[...all]]/route";
 import { hc } from "hono/client";
 
 export const rpc = hc<AppRouter>(process.env.NEXT_PUBLIC_APP_URL!);
