183cd0fe0cba8a89bbc6c4238821d719c26b0c61
merthanmerter <merthanm@gmail.com>
2025-04-23 22:06:21 +0300

    Enable locale detection in internationalization routing configuration to enhance user experience by automatically selecting the appropriate language based on user preferences.

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -3,6 +3,7 @@ import { defineRouting } from "next-intl/routing";
 export const routing = defineRouting({
   locales: ["en", "tr"],
   defaultLocale: "en",
+  localeDetection: true,
   pathnames: {
     "/": "/",
     "/test": {
