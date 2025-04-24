425bac95f18672c807ba275d08c72c981a529718
merthanmerter <merthanm@gmail.com>
2025-04-24 07:02:29 +0300

    Refactor TestPage component to integrate query client hydration for improved data fetching, reintroduce HydrationBoundary for better SSR support, and clean up commented-out code for clarity.

--- src/app/[locale]/test/page.tsx
+++ src/app/[locale]/test/page.tsx
@@ -1,57 +1,57 @@
 import RefetchHello from "@/components/refetch-hello";
 import RpcResponse from "@/components/rpc-response";
 import SendTestEmail from "@/components/send-test-email";
+import { getQueryClient } from "@/lib/query-client";
 import { getRpcClient } from "@/lib/rpc";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { getTranslations } from "next-intl/server";
 import "server-only";
-// import { getQueryClient } from "@/lib/query-client";
-// import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 
 export default async function Page() {
   const t = await getTranslations("TestPage");
-  // const queryClient = getQueryClient();
-  // await queryClient.prefetchQuery({
-  //   queryKey: ["rpcResponseSSR"],
-  //   queryFn: async () => {
-  //     const rpc = await getRpcClient();
-  //     const res = await rpc.api.hello.$get({
-  //       query: { message: "from hono ssr!" },
-  //     });
-  //     return res.json();
-  //   },
-  // });
-
-  const rpc = await getRpcClient();
-  const res = await rpc.api.hello.$get({
-    query: { message: "from hono ssr!" },
+  const queryClient = getQueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ["rpcResponseSSR"],
+    queryFn: async () => {
+      const rpc = await getRpcClient();
+      const res = await rpc.api.hello.$get({
+        query: { message: "from hono ssr!" },
+      });
+      return res.json();
+    },
   });
-  const initialData = await res.json();
+
+  // const rpc = await getRpcClient();
+  // const res = await rpc.api.hello.$get({
+  //   query: { message: "from hono ssr!" },
+  // });
+  // const initialData = await res.json();
 
   return (
-    // <HydrationBoundary state={dehydrate(queryClient)}>
-    <div>
-      <h1 className='text-2xl font-bold font-mono text-center mx-auto'>
-        {t("title")}
-      </h1>
-      <div className='text-sm text-center mx-auto'>
-        {t.rich("description", {
-          p: (chunks) => <p className='mt-4'>{chunks}</p>,
-          code: (chunks) => (
-            <code className='font-mono text-muted-foreground'>{chunks}</code>
-          ),
-        })}
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <div>
+        <h1 className='text-2xl font-bold font-mono text-center mx-auto'>
+          {t("title")}
+        </h1>
+        <div className='text-sm text-center mx-auto'>
+          {t.rich("description", {
+            p: (chunks) => <p className='mt-4'>{chunks}</p>,
+            code: (chunks) => (
+              <code className='font-mono text-muted-foreground'>{chunks}</code>
+            ),
+          })}
 
-        <RpcResponse
-          initialData={initialData}
-          queryKey={["rpcResponseSSR"]}
-        />
-        <RpcResponse queryKey={["rpcResponseCSR"]} />
-        <div className='flex gap-2 mt-4 justify-center items-center'>
-          <RefetchHello />
-          <SendTestEmail />
+          <RpcResponse
+            // initialData={initialData}
+            queryKey={["rpcResponseSSR"]}
+          />
+          <RpcResponse queryKey={["rpcResponseCSR"]} />
+          <div className='flex gap-2 mt-4 justify-center items-center'>
+            <RefetchHello />
+            <SendTestEmail />
+          </div>
         </div>
       </div>
-    </div>
-    // </HydrationBoundary>
+    </HydrationBoundary>
   );
 }
